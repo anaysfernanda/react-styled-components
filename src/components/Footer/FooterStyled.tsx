86fc57c98e66959fb10eb0ef8ea01d6b7265de99
Fernanda de Souza <a.fernandadesouza@gmail.com>
2022-10-31 22:53:56 -0300

    prática de styled component

---
+++ src/components/Footer/FooterStyled.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const DivFooterStyled = styled.div`
+  height: 200px;
+  background-color: rgb(221, 221, 221);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const ImgIconStyled = styled.img`
+  width: 25px;
+`;
+
+const DivLinkStyled = styled.div`
+  display: flex;
+`;
+
+const DivIconStyled = styled.div`
+  display: flex;
+`;
+
+export { DivFooterStyled, ImgIconStyled, DivLinkStyled, DivIconStyled };
